e444c24c030a9f2ca95b575448a52d593a29868a
iacobDelia <deliaiacob89@gmail.com>
2025-12-19 13:44:12 +0200

    get info about the contracts

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 
 
+
 ## Useful
 SysNoticeTypeids:
 - 3:  Anunt de atribuire la Anunt de participare (CN) 
@@ -11,8 +12,10 @@ SysNoticeTypeids:
 - 20:  Anunt de atribuire la invitatia de depunere oferta la sistemul de achizitie dinamic (RFDA) 
 
 ## Notes
-There is also a field ```noticeAwardCriteriaList``` that contains the weights of the reasons for choosing a bidder. For example:
-- Price: 60%
-- Warranty: 20%
-- Quality: 20%
-I chose not to include it because it would require a separate table and it isn't that relevant for statistics.
+- There is also a field ```noticeAwardCriteriaList``` that the script doesn't save. It contains the weights of the reasons for choosing a bidder. For example:
+    - Price: 60%
+    - Warranty: 20%
+    - Quality: 20%
+
+- There are instances of an association of companies winning a single contract, the script only saves the leader
+
--- seap_requests.py
+++ seap_requests.py
@@ -5,8 +5,11 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 from datetime import datetime
 from zoneinfo import ZoneInfo
+import os
 
-# get detailed info for a notice
+
+
+# get detailed info for a contract award
 def get_info_CANotice(id):
     headers = {'Content-Type': 'application/json;charset=UTF-8',
                'Referer': 'https://e-licitatie.ro/pub/notices/contract-notices/list/0/0'}
@@ -99,9 +102,102 @@ def send_req():
             break
     
     data_dict_table = pa.Table.from_pylist(final_list)
-    pq.write_table(data_dict_table, 'temp.parquet')
+    pq.write_table(data_dict_table, 'contract_awards.parquet')
 
-    print(pq.read_schema('temp.parquet'))
+    print(pq.read_schema('contract_awards.parquet'))
+
+# removes all other characters from the CUI besides the numerical ones
+def clean_CUI(CUI):
+    if not CUI:
+        return None
+    return ''.join(filter(str.isdigit, CUI))
+
+def get_contract_info(caNoticeId):
+    headers = {'Content-Type': 'application/json;charset=UTF-8',
+               'Referer': 'https://e-licitatie.ro/pub/notices/contract-notices/list/0/0'}
+    url = 'http://e-licitatie.ro/api-pub/C_PUBLIC_CANotice/GetCANoticeContracts'
+    body = {
+    "caNoticeId": caNoticeId,
+    "contractNo": None,
+    "winnerTitle": None,
+    "winnerFiscalNumber": None,
+    "contractDate": {
+        "from": None,
+        "to": None
+    },
+    "contractValue": {
+        "from": None,
+        "to": None
+    },
+    "contractMinOffer": {
+        "from": None,
+        "to": None
+    },
+    "contractMaxOffer": {
+        "from": None,
+        "to": None
+    },
+    "contractTitle": None,
+    "lots": None,
+    "sortOrder": [],
+    "sysContractFrameworkType": {},
+    "skip": 0,
+    "take": 5
+    }
+    r = requests.post(url, headers = headers, json = body, timeout = 5)
+    return r.json()['items']
+
+def get_contracts():
+    if not os.path.exists('contract_awards.parquet'):
+        print("File 'contract_awards.parquet' could not be found.")
+        return
+    if os.path.exists('contractors.parquet'):
+        pq_table_contractors = pq.read_table('contractors.parquet', columns = ["CUI"])
+        CUI_set = set(pq_table_contractors['CUI'].to_pylist())
+    else:
+        CUI_set = set()
+    
+    pq_table_contracts =  pq.read_table('contract_awards.parquet', columns = ["caNoticeId"])
+    new_contractors_list = []
+    new_contracts_list = []
+
+    for id in pq_table_contracts['caNoticeId']:
+        caNoticeId = id.as_py()
+        contract_items = get_contract_info(str(caNoticeId))
+        print(f"Now saving info for caNoticeId {caNoticeId}")
+        for contract in contract_items:
+            winnerCUI = clean_CUI(contract['winner']['fiscalNumber'])
+            final_contract = {
+                'caNoticeContractId': contract['caNoticeContractId'],
+                'caNoticeId': contract['caNoticeId'],
+                'contractTitle': contract['contractTitle'],
+                'contractDate': convert_date(contract['contractDate']),
+                'winnerCUI': winnerCUI,
+                'contractValue': contract['defaultCurrencyContractValue']
+            }
+            new_contracts_list.append(final_contract)
+            
+            if(winnerCUI not in CUI_set):
+                address = contract['winner']['address']
+                new_contractor = {
+                    'CUI': winnerCUI,
+                    'officialName': address['officialName'],
+                    'city': address['city'],
+                    'country': address['country'],
+                    'isSME': address['isSME']
+                }
+                CUI_set.add(winnerCUI)
+                new_contractors_list.append(new_contractor)
+        time.sleep(2)
+    
+    new_contracts_table = pa.Table.from_pylist(new_contracts_list)
+    pq.write_table(new_contracts_table, 'contracts.parquet')
+    new_contractors_table = pa.Table.from_pylist(new_contractors_list)
+    pq.write_table(new_contractors_table, 'contractors.parquet')
+
+    print(pq.read_schema('contract_awards.parquet'))
+    print(pq.read_schema('contractors.parquet'))
 
 if __name__ == "__main__":
-    send_req()
+    #send_req()
+    get_contracts()
